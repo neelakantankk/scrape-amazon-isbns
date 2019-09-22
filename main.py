141e1b4e485c71070d953308a815016631f6a620
Neelakantan K.K <neelakantankk@Terrapin-Station.Candyman>
2019-09-22 18:55:05 +0530

    Removed call to search; removed amazon_item.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import logging.config
 import json
 from random import choice
 from sys import exit as sysExit
-from amazon_item import Amazon_item
 
 #---------------------Required Amazon URLs-----------------------------------#
 amazon_urls = {
@@ -64,39 +63,6 @@ def main():
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
             }
 
-    search_params = {'k':f'{choice(trial_list_of_ISBNs)}'}
-
-    logger.debug(f'Calling {amazon_urls["search"]} with params {search_params}')
-
-    search_results = session.get(amazon_urls['search'],
-                                 params=search_params)
-
-    if search_results.status_code != 200:
-        logger.error(f'Search returned search_results.status_code')
-        sysExit(1)
-    else:
-        logger.debug('Amazon returned page')
-        with pathlib.Path.cwd().joinpath('file-dump/search_page.html').open('wb') as fObject:
-            fObject.write(search_results.content)
-
-    amazon_page = lhtml.fromstring(search_results.content)
-
-    if not amazon_page.findall('.//div[@data-asin]'):
-        logger.debug("No search results")
-
-    amazon_items = []
-    for div in amazon_page.findall('.//div[@data-asin]'):
-        logger.debug(f'ASIN = {div.get("data-asin")}')
-        logger.debug(f'Product name : {"".join(div.xpath(".//h2/a//text()")).strip()}')
-        logger.debug(f'Link to product page: {div.find(".//h2/a").get("href")}')
-        amazon_items.append(Amazon_item(div.get("data-asin"),
-            div.find(".//h2/a").get("href")))
-
-    for item in amazon_items:
-        logger.debug(f"Product page for ASIN {item.asin} is {item.product_page}")
-        loger.debug(f"Calling Product Page")
-
-        product_page = item.get_page()
 
 
 
